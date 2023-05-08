8e7d035335e62ffde080f4eee9e5ae74d40bd449
magalhaesmth <matheusurbano2002@hotmail.com>
2023-05-08 18:22:32 -0300

    Faltando tela de listar

--- cadastros/urls.py
+++ cadastros/urls.py
@@ -1,17 +1,53 @@
 from django.urls import path
-from .views import FornecedorCreate, FornecedorUpdate, FornecedorList, FornecedorDelete, FornecedorDetail
+from .views import CategoriaCreate, CategoriaDelete, CategoriaDetail, CategoriaList, CategoriaUpdate, ClienteCreate, ClienteDelete, ClienteDetail, ClienteList, ClienteUpdate, FabricanteCreate, FabricanteDelete, FabricanteDetail, FabricanteList, FabricanteUpdate, FornecedorCreate, FornecedorUpdate, FornecedorList, FornecedorDelete, FornecedorDetail, FuncionarioCreate, FuncionarioDelete, FuncionarioDetail, FuncionarioList, FuncionarioUpdate, MarcaCreate, MarcaDelete, MarcaDetail, MarcaList, MarcaUpdate, ProdutoCreate, ProdutoDelete, ProdutoDetail, ProdutoList, ProdutoUpdate, VendaCreate, VendaDelete, VendaDetail, VendaList, VendaUpdate
 
 
 urlpatterns = [
 
     path("cadastrar/fornecedor/", FornecedorCreate.as_view(), name="cadastrar-fornecedor"),
+    path("cadastrar/fabricante/", FabricanteCreate.as_view(), name="cadastrar-fabricante"),
+    path("cadastrar/marca/", MarcaCreate.as_view(), name="cadastrar-marca"),
+    path("cadastrar/categoria/", CategoriaCreate.as_view(), name="cadastrar-categoria"),
+    path("cadastrar/cliente/", ClienteCreate.as_view(), name="cadastrar-cliente"),
+    path("cadastrar/funcionario/", FuncionarioCreate.as_view(), name="cadastrar-funcionario"),
+    path("cadastrar/produto/", ProdutoCreate.as_view(), name="cadastrar-produto"),
+    path("cadastrar/venda/", VendaCreate.as_view(), name="cadastrar-venda"),
 
     path("editar/fornecedor/<int:pk>/", FornecedorUpdate.as_view(), name="editar-fornecedor"),
+    path("editar/fabricante/<int:pk>/", FabricanteUpdate.as_view(), name="editar-fabricante"),
+    path("editar/marca/<int:pk>/", MarcaUpdate.as_view(), name="editar-marca"),
+    path("editar/categoria/<int:pk>/", CategoriaUpdate.as_view(), name="editar-categoria"),
+    path("editar/cliente/<int:pk>/", ClienteUpdate.as_view(), name="editar-cliente"),
+    path("editar/funcionario/<int:pk>/", FuncionarioUpdate.as_view(), name="editar-funcionario"),
+    path("editar/produto/<int:pk>/", ProdutoUpdate.as_view(), name="editar-produto"),
+    path("editar/venda/<int:pk>/", VendaUpdate.as_view(), name="editar-venda"),
 
     path("excluir/fornecedor/<int:pk>/", FornecedorDelete.as_view(), name="excluir-fornecedor"),
+    path("excluir/fabricante/<int:pk>/", FabricanteDelete.as_view(), name="excluir-fabricante"),
+    path("excluir/marca/<int:pk>/", MarcaDelete.as_view(), name="excluir-marca"),
+    path("excluir/categoria/<int:pk>/", CategoriaDelete.as_view(), name="excluir-categoria"),
+    path("excluir/cliente/<int:pk>/", ClienteDelete.as_view(), name="excluir-cliente"),
+    path("excluir/funcionario/<int:pk>/", FuncionarioDelete.as_view(), name="excluir-funcionario"),
+    path("excluir/produto/<int:pk>/", ProdutoDelete.as_view(), name="excluir-produto"),
+    path("excluir/venda/<int:pk>/", VendaDelete.as_view(), name="excluir-venda"),
 
     path("listar/fornecedor/", FornecedorList.as_view(), name="listar-fornecedor"),
+    path("listar/fabricante/", FabricanteList.as_view(), name="listar-fabricante"),
+    path("listar/marca/", MarcaList.as_view(), name="listar-marca"),
+    path("listar/categoria/", CategoriaList.as_view(), name="listar-categoria"),
+    path("listar/cliente/", ClienteList.as_view(), name="listar-cliente"),
+    path("listar/funcionario/", FuncionarioList.as_view(), name="listar-funcionario"),
+    path("listar/produto/", ProdutoList.as_view(), name="listar-produto"),
+    path("listar/venda/", VendaList.as_view(), name="listar-venda"),
 
     path("detalhar/fornecedor/<int:pk>/",
          FornecedorDetail.as_view(), name="detalhar-fornecedor"),
+    path("detalhar/fabricante/<int:pk>/", FabricanteDetail.as_view(), name="detalhar-fabricante"),
+    path("detalhar/marca/<int:pk>/", MarcaDetail.as_view(), name="detalhar-marca"),
+    path("detalhar/categoria/<int:pk>/", CategoriaDetail.as_view(), name="detalhar-categoria"),
+    path("detalhar/cliente/<int:pk>/", ClienteDetail.as_view(), name="detalhar-cliente"),
+    path("detalhar/funcionario/<int:pk>/", FuncionarioDetail.as_view(), name="detalhar-funcionario"),
+    path("detalhar/produto/<int:pk>/", ProdutoDetail.as_view(), name="detalhar-produto"),
+    path("detalhar/venda/<int:pk>/", VendaDetail.as_view(), name="detalhar-venda"),
+
 ]
--- cadastros/views.py
+++ cadastros/views.py
@@ -1,4 +1,4 @@
-from .models import Fornecedor
+from .models import Categoria, Cliente, Fabricante, Fornecedor, Funcionario, Marca, Produto, Venda
 from django.urls import reverse_lazy
 
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
@@ -17,22 +17,220 @@ class FornecedorCreate(LoginRequiredMixin, CreateView):
     extra_context = {"titulo": "Cadastro de Fornecedores"}
 
 
+    def get_context_data(self, *args, **kwargs):
+        dados = super().get_context_data(*args, **kwargs)
+        dados["titulo"] = "Cadastro de Fornecedor"
+        return dados
+
+class FabricanteCreate(CreateView):
+    model = Fabricante
+    fields = ["nome"]
+    template_name = "cadastros/form.html"
+    success_url = reverse_lazy("listar-fabricante") #Editar aqui depois para retornar a pagina inicial
+    extra_context = {"titulo": "Cadastro de Fabricante"}
+
+class MarcaCreate(CreateView):
+    model = Marca
+    fields = ["nome", "fornecedor"]
+    template_name = "cadastros/form.html"
+    success_url = reverse_lazy("listar-marca")
+    extra_context = {"titulo": "Cadastro de Marca"}
+
+class CategoriaCreate(CreateView):
+    model = Categoria
+    fields = ["nome"]
+    template_name = "cadastros/form.html"
+    success_url = reverse_lazy("listar-categoria")
+    extra_context = {"titulo": "Cadastro de Categoria"}
+
+
+class ClienteCreate(CreateView):
+    model = Cliente
+    fields = ["nome", "cpfCnpj", "telefone", "endereco"]
+    template_name = "cadastros/form.html"
+    success_url = reverse_lazy("listar-cliente")
+    extra_context = {"titulo": "Cadastro de Cliente"}
+
+class FuncionarioCreate(CreateView):
+    model = Funcionario
+    fields = ["nome", "email", "funcao", "endereco", "telefone"]
+    template_name = "cadastros/form.html"
+    success_url = reverse_lazy("listar-funcionario")
+    extra_context = {"titulo": "Cadastro de Funcionario"}
+
+class ProdutoCreate(CreateView):
+    model = Produto
+    fields = ["nome", "valor", "codigo", "fornecedor", "marca", "categoria"]
+    template_name = "cadastros/form.html"
+    success_url = reverse_lazy("listar-produto")
+    extra_context = {"titulo": "Cadastro de Produto"}
+
+class VendaCreate(CreateView):
+    model = Venda
+    fields = ["produto", "cliente", "quantidade"]
+    template_name = "cadastros/form.html"
+    success_url = reverse_lazy("listar-venda")
+    extra_context = {"titulo": "Cadastro de Venda"}
+
+
+#######################################################################################
+
 class FornecedorUpdate(UpdateView):
     model = Fornecedor
     fields = ["nome", "telefone"]
     template_name = "cadastros/form.html"
     success_url = reverse_lazy("listar-fornecedor")
 
+class FabricanteUpdate(UpdateView):
+    model = Fabricante
+    fields = ["nome"]
+    template_name = "cadastros/form.html"
+    success_url = reverse_lazy("listar-fabricante")
 
-class FornecedorDelete(DeleteView):
+class MarcaUpdate(UpdateView):
+    model = Marca
+    fields = ["nome", "fornecedor"]
+    template_name = "cadastros/form.html"
+    success_url = reverse_lazy("listar-marca")
+
+class CategoriaUpdate(UpdateView):
+    model = Categoria
+    fields = ["nome"]
+    template_name = "cadastros/form.html"
+    success_url = reverse_lazy("listar-categoria")
+
+class ClienteUpdate(UpdateView):
+    model = Cliente
+    fields = ["nome", "cpfCnpj", "telefone", "endereco"]
+    template_name = "cadastros/form.html"
+    success_url = reverse_lazy("listar-cliente")
+
+class FuncionarioUpdate(UpdateView):
+    model = Funcionario
+    fields = ["nome", "email", "funcao", "endereco", "telefone"]
+    template_name = "cadastros/form.html"
+    success_url = reverse_lazy("listar-funcionario")
+
+class ProdutoUpdate(UpdateView):
+    model = Produto
+    fields = ["nome", "valor", "codigo", "fornecedor", "marca", "categoria"]
+    template_name = "cadastros/form.html"
+    success_url = reverse_lazy("listar-produto")
+
+class VendaUpdate(UpdateView):
+    model = Venda
+    fields = ["produto", "cliente", "quantidade"]
+    template_name = "cadastros/form.html"
+    success_url = reverse_lazy("listar-venda")
+
+
+######################################################################################
+
+class FornecedorDelete(DeleteView):      
     model = Fornecedor
     template_name = "cadastros/delete.html"
-    success_url = reverse_lazy("deletar-fornecedor")
+    success_url = reverse_lazy("listar-fornecedor")
+
+class FabricanteDelete(DeleteView):
+    model = Fornecedor
+    template_name = "cadastros/delete.html"
+    success_url = reverse_lazy("listar-fabricante")
+
+class MarcaDelete(DeleteView):
+    model = Marca
+    template_name = "cadastros/delete.html"
+    success_url = reverse_lazy("listar-marca")
+
+class CategoriaDelete(DeleteView):
+    model = Categoria
+    template_name = "cadastros/delete.html"
+    success_url = reverse_lazy("listar-categoria")
+
+class ClienteDelete(DeleteView):
+    model = Cliente
+    template_name = "cadastros/delete.html"
+    success_url = reverse_lazy("listar-cliente")
+
+class FuncionarioDelete(DeleteView):
+    model = Funcionario
+    template_name = "cadastros/delete.html"
+    success_url = reverse_lazy("listar-funcionario")
+
+class ProdutoDelete(DeleteView):
+    model = Produto
+    template_name = "cadastros/delete.html"
+    success_url = reverse_lazy("listar-produto")
+
+class VendaDelete(DeleteView):
+    model = Venda
+    template_name = "cadastros/delete.html"
+    success_url = reverse_lazy("listar-venda")
+
+
+######################################################################################
 
 class FornecedorList(ListView):
     model = Fornecedor
     template_name = "cadastros/list/fornecedor.html"
 
+class FabricanteList(ListView):
+    model = Fornecedor
+    template_name = "cadastros/list/fabricante.html"
+
+class MarcaList(ListView):
+    model = Marca
+    template_name = "cadastros/list/marca.html"
+
+class CategoriaList(ListView):
+    model = Categoria
+    template_name = "cadastros/list/categoria.html"
+
+class ClienteList(ListView):
+    model = Cliente
+    template_name = "cadastros/list/cliente.html"
+
+class FuncionarioList(ListView):
+    model = Funcionario
+    template_name = "cadastros/list/funcionario.html"
+
+class ProdutoList(ListView):
+    model = Produto
+    template_name = "cadastros/list/produto.html"
+
+class VendaList(ListView):
+    model = Venda
+    template_name = "cadastros/list/venda.html"
+
+######################################################################################
+
 class FornecedorDetail(DetailView):
     model = Fornecedor
     template_name = "cadastros/detail/Fornecedor.html"
+
+class FabricanteDetail(DetailView):
+    model = Fornecedor
+    template_name = "cadastros/detail/fabricante.html"
+
+class MarcaDetail(DetailView):
+    model = Marca
+    template_name = "cadastros/detail/marca.html"
+
+class CategoriaDetail(DetailView):
+    model = Categoria
+    template_name = "cadastros/detail/categoria.html"
+
+class ClienteDetail(DetailView):
+    model = Cliente
+    template_name = "cadastros/detail/cliente.html"
+
+class FuncionarioDetail(DetailView):
+    model = Funcionario
+    template_name = "cadastros/detail/funcionario.html"
+
+class ProdutoDetail(DetailView):
+    model = Produto
+    template_name = "cadastros/detail/produto.html"
+
+class VendaDetail(DetailView):
+    model = Venda
+    template_name = "cadastros/detail/venda.html"
