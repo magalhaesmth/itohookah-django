d8660b6c6b049be39af6c9f23193737a7a0f327a
magalhaesmth <matheusurbano2002@hotmail.com>
2023-11-30 20:20:11 -0300

    Tela de relatórios básica

--- paginas/urls.py
+++ paginas/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import IndexView, SobreView
+from .views import IndexView, SobreView, RelatoriosView
 
 urlpatterns = [
     path("", IndexView.as_view(), name="pagina-inicial"),
 
     path("sobre", SobreView.as_view(), name="sobre"),
+
+    path("relatorios", RelatoriosView.as_view(), name="relatorios"),
 ]
--- paginas/views.py
+++ paginas/views.py
@@ -1,9 +1,31 @@
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from datetime import datetime
+from cadastros.models import Categoria, Cliente, Fabricante, Fornecedor, Funcionario, Marca, Produto, Venda
 
 class IndexView(LoginRequiredMixin, TemplateView):
     template_name = "paginas/index.html"
 
-
 class SobreView(LoginRequiredMixin, TemplateView):
     template_name = "paginas/sobre.html"
+
+class RelatoriosView(LoginRequiredMixin, TemplateView):
+    template_name = "paginas/relatorios.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context["categorias"] = Categoria.objects.filter(
+            cadastrado_em__month=datetime.now().month).count()
+        context["clientes"] = Cliente.objects.filter(
+            cadastrado_em__month=datetime.now().month).count()
+        context["marcas"] = Marca.objects.filter(
+            cadastrado_em__month=datetime.now().month).count()
+        context["fabricantes"] = Fabricante.objects.filter(
+            cadastrado_em__month=datetime.now().month).count()
+        context["forncedores"] = Fornecedor.objects.filter(
+            cadastrado_em__month=datetime.now().month).count()
+        context["produtos"] = Produto.objects.filter(
+            cadastrado_em__month=datetime.now().month).count()
+
+        return context
