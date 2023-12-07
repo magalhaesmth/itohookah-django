4360922edf2abc263593476205c34966536ce1ea
magalhaesmth <matheusurbano2002@hotmail.com>
2023-12-06 22:12:49 -0300

    Correção do logout  e tela de registro de usuário

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
-from django.contrib.auth import views
+from django.contrib.auth import views as auth_views
+from .views import UsuarioCreate
 
 urlpatterns = [
-    path("login/", views.LoginView.as_view(
+    path("login/", auth_views.LoginView.as_view(
         template_name="cadastros/form-login.html",
         extra_context={"descricao_botao": "Entrar"}
     ), name="login"),
 
-    path("logout/", views.LogoutView.as_view(), name="logout"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+
+    path('registrar/', UsuarioCreate.as_view(), name='registrar'),
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from django.contrib.auth.models import User
+from .forms import UsuarioForm
+from django.urls import reverse_lazy
 
-# Create your views here.
+class UsuarioCreate(CreateView):
+    template_name = 'cadastros/form.html'
+    form_class = UsuarioForm
+    success_url = reverse_lazy('login')
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+
+        context['titulo'] = "Cadastro de novo usuário"
+
+        return context
